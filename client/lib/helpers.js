81339d33f8bd882aef58b8fed4ae0765263c5b5c
zwrose <zwrose@gmail.com>
2015-02-01 02:17:36 +0000

    initial framework in place

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -1,3 +1,5 @@
+Session.set("alreadyRSVPd", false);
+
 Template.body.helpers({
   friAttendees: function () {
     return Attendees.find({fri: true});
@@ -7,5 +9,31 @@ Template.body.helpers({
   },
   sunAttendees: function () {
     return Attendees.find({sun: true});
+  },
+  nonAttendees: function () {
+    return Attendees.find({sun: false, sat:false, fri: false});
+  },
+  checkAlreadyRSVPd: function() {
+    return !Session.get("alreadyRSVPd");
   }
 });
+
+Template.insertRSVP.events({
+  "click .already": function (event) {
+    return Session.set("alreadyRSVPd", true);
+  }
+//   "submit": function (event) {
+//     return Session.set("alreadyRSVPd", true);
+//   }
+});
+
+Template.iveAlreadyRSVPd.events({
+  "click .again": function (event) {
+    return Session.set("alreadyRSVPd", false);
+  }
+});
+AutoForm.addHooks("insertRSVP", {
+  onSuccess: function() {
+    return Session.set("alreadyRSVPd", true);
+  }
+})
